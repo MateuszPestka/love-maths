2c35a63bea4fca4740f3731845fc4c8c53774139
MateuszPestka <pes.matthew@gmail.com>
2022-03-04 11:50:07 +0000

    s

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaysubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
